a305aac779d07001f4208bc31f1c803c40a7c7f2
JMarte03 <collegeis2much@gmail.com>
2025-01-16 19:45:14 -0400

    Projects session

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,20 @@ module.exports = {
     screens: {
       sm: "480px",
       md: "768px",
-      lg: "976px",
+      lg: "1055px",
       xl: "1440px",
     },
     extend: {
       // costumized colors
       colors: {
-        greyishGreen: '#F4F8FC'
+        greyishGreen: '#F4F8FC',
+        darkGreen: '#1C6665',
+        lightText: '#606773',
+        darkText: '#070707',
+        lightGray: '#E3E6EA',
+        lightGreen: '#D0FA94',
+        orange: '#FE8259',
+        brown: '#302A26'
       }
     },
   },
